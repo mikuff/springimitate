6a9028d79c9cbc572bcfd1fcbaf111a934596913
miku fufu <2330016764@qq.com>
2022-10-15 16:59:34 +0800

    初始化提交 spring-beans 模块

---
+++ spring-beans/src/main/java/exception/BeansException.java
@@ -0,0 +1,16 @@
+package exception;
+
+/**
+ * Bean处理相关的异常
+ */
+public class BeansException extends RuntimeException {
+
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+    public BeansException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
